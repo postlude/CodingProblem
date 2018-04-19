7d023d99328642350725d8e64dad7e48d54a52ef
postlude <seungdeokhan@naver.com>
2018-04-20 01:19:11 +0900

    p9663, p9663_2 완료. p2580 진행중

---
+++ src/baekjoon/algorithmcourse/P2580.java
@@ -0,0 +1,148 @@
+package baekjoon.algorithmcourse;
+
+import java.util.Scanner;
+
+/**
+ * 스도쿠
+ * 
+ * 문제
+ * 스도쿠는 18세기 스위스 수학자가 만든 '라틴 사각형'이랑 퍼즐에서 유래한 것으로 현재 많은 인기를 누리고 있다.
+ * 이 게임은 아래 그림과 같이 가로, 세로 각각 9개씩 총 81개의 작은 칸으로 이루어진 정사각형 판 위에서 이뤄지는데, 게임 시작 전 몇 몇 칸에는 1부터 9까지의 숫자 중 하나가 쓰여 있다.
+ * 나머지 빈 칸을 채우는 방식은 다음과 같다.
+ * 
+ * 각각의 가로줄과 세로줄에는 1부터 9까지의 숫자가 한 번씩만 나타나야 한다.
+ * 굵은 선으로 구분되어 있는 3x3 정사각형 안에도 1부터 9까지의 숫자가 한 번씩만 나타나야 한다.
+ * 위의 예의 경우, 첫째 줄에는 1을 제외한 나머지 2부터 9까지의 숫자들이 이미 나타나 있으므로 첫째 줄 빈칸에는 1이 들어가야 한다.
+ * 
+ * 또한 위쪽 가운데 위치한 3x3 정사각형의 경우에는 3을 제외한 나머지 숫자들이 이미 쓰여있으므로 가운데 빈 칸에는 3이 들어가야 한다.
+ * 
+ * 이와 같이 빈 칸을 차례로 채워 가면 다음과 같은 최종 결과를 얻을 수 있다.
+ * 게임 시작 전 스도쿠 판에 쓰여 있는 숫자들의 정보가 주어질 때 모든 빈 칸이 채워진 최종 모습을 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 아홉 줄에 걸쳐 한 줄에 9개씩 게임 시작 전 스도쿠판 각 줄에 쓰여 있는 숫자가 한 칸씩 띄워서 차례로 주어진다. 스도쿠 판의 빈 칸의 경우에는 0이 주어진다. 스도쿠 판을 규칙대로 채울 수 없는 경우의 입력은 주어지지 않는다.
+ * 
+ * 출력
+ * 모든 빈 칸이 채워진 스도쿠 판의 최종 모습을 아홉줄에 걸쳐 한 줄에 9개씩 한 칸씩 띄워서 출력한다.
+ * 스도쿠 판을 채우는 방법이 여럿인 경우는 그 중 하나만을 출력한다.
+ * 
+ * 
+ * 예제 입력 1
+ * 0 3 5 4 6 9 2 7 8
+ * 7 8 2 1 0 5 6 0 9
+ * 0 6 0 2 7 8 1 3 5
+ * 3 2 1 0 4 6 8 9 7
+ * 8 0 4 9 1 3 5 0 6
+ * 5 9 6 8 2 0 4 1 3
+ * 9 1 7 6 5 2 0 8 0
+ * 6 0 3 7 0 1 9 5 2
+ * 2 5 8 3 9 4 7 6 0
+ * 
+ * 
+ * 예제 출력 1
+ * 1 3 5 4 6 9 2 7 8
+ * 7 8 2 1 3 5 6 4 9
+ * 4 6 9 2 7 8 1 3 5
+ * 3 2 1 5 4 6 8 9 7
+ * 8 7 4 9 1 3 5 2 6
+ * 5 9 6 8 2 7 4 1 3
+ * 9 1 7 6 5 2 3 8 4
+ * 6 4 3 7 8 1 9 5 2
+ * 2 5 8 3 9 4 7 6 1
+ * 
+ */
+public class P2580 {
+	static int[][] SUDOKU = new int[9][9];  
+	static boolean[][] CHECK_ROW = new boolean[9][9];
+	static boolean[][] CHECK_COL= new boolean[9][9];
+	static boolean[][] CHECK_SQUARE = new boolean[9][9];
+	
+	static int NUM_OF_ZERO = 0;
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		for(int row=0; row<9; row++) {
+			for(int col=0; col<9; col++) {
+				int num = scan.nextInt();
+				P2580.SUDOKU[row][col] = num;
+				
+				if(num != 0) {
+					P2580.CHECK_ROW[row][num-1] = true;
+					P2580.CHECK_COL[num-1][col] = true;
+					P2580.CHECK_SQUARE[row/3*3+col/3][num-1] = true;
+				}else {
+					P2580.NUM_OF_ZERO++;
+				}
+			}
+		}
+		scan.close();
+		// input end
+		
+		
+		P2580 p2580 = new P2580();
+		p2580.solveSudoku(0);
+	}
+
+	public void printArray() {
+		for(int row=0; row<9; row++) {
+			for(int col=0; col<9; col++) {
+				System.out.print(P2580.SUDOKU[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public boolean isCollect(int row, int col, int num) {
+		// 가로
+		if(P2580.CHECK_ROW[row][num-1]) {
+			return false;
+		}
+		
+		// 세로
+		if(P2580.CHECK_COL[num-1][col]) {
+			return false;
+		}
+		
+		// 작은 사각형
+		if(P2580.CHECK_SQUARE[row/3*3+col/3][num-1]) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public void solveSudoku(int row) {
+		if(row == 9) {
+			if(P2580.NUM_OF_ZERO == 0) {
+				printArray();
+				System.out.println("--------------------------");
+			}
+			return;
+		}
+		
+		
+		for(int col=0; col<9; col++) {
+			if(P2580.SUDOKU[row][col] != 0) {
+				continue;
+			}
+		
+			for(int num=1; num<=9; num++) {
+				if(isCollect(row, col, num)) {
+					P2580.SUDOKU[row][col] = num;
+					P2580.CHECK_ROW[row][num-1] = true;
+					P2580.CHECK_COL[num-1][col] = true;
+					P2580.CHECK_SQUARE[row/3*3+col/3][num-1] = true;
+					P2580.NUM_OF_ZERO--;
+					
+					solveSudoku(row+1);
+					
+					P2580.SUDOKU[row][col] = 0;
+					P2580.CHECK_ROW[row][num-1] = false;
+					P2580.CHECK_COL[num-1][col] = false;
+					P2580.CHECK_SQUARE[row/3*3+col/3][num-1] = false;
+					P2580.NUM_OF_ZERO++;
+				}
+			}
+		}
+	}
+}
--- src/baekjoon/algorithmcourse/P9663.java
+++ src/baekjoon/algorithmcourse/P9663.java
@@ -24,35 +24,145 @@ import java.util.Scanner;
  * 
  */
 public class P9663 {
-	static int[][] CHESS;
+	static boolean[][] CHESS;
+	static int N;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
+		P9663.N = scan.nextInt();
 		scan.close();
+		P9663.CHESS = new boolean[N][N];
 		
-		P9663.CHESS = new int[N][N];
+		P9663 p9663 = new P9663();
+		System.out.println(p9663.calcQueen(0));
 	}
 	
-	public boolean setQueen(int row, int col, int queen) {
+	public boolean isPossiblePosition(int row, int col) {
+		// 가로 세로
+		/*for(int index=0; index<P9663.N; index++) {
+			if(P9663.CHESS[row][index]) {
+				return false;
+			}
+			if(P9663.CHESS[index][col]) {
+				return false;
+			}
+		}*/
 		
+		// calcQueen() 메소드에서 row, col이 차례로 증가하는 순서로 검사하므로 가로는 검사할 필요가 없고
+		// 세로도 현재 row, col보다 위에 있는 것만 검사하면 된다.
+		
+		// 세로 위
+		for(int index=0; index<row; index++) {
+			if(P9663.CHESS[index][col]) {
+				return false;
+			}
+		}
+		
+		
+		// 대각선
+		/*int calcIndex = 1;
+		boolean rightDown = true;
+		boolean leftUp = true;
+		boolean rightUp = true;
+		boolean leftDown = true;
+		while(rightDown || leftUp || rightUp || leftDown) {
+			// \ 대각선
+			// 우하단
+			if(rightDown && row+calcIndex<P9663.N && col+calcIndex<P9663.N) {
+				if(P9663.CHESS[row+calcIndex][col+calcIndex]) {
+					return false;
+				}
+			}else {
+				rightDown = false;
+			}
+			
+			// 좌상단
+			if(leftUp && row-calcIndex>=0 && col-calcIndex>=0) {
+				if(P9663.CHESS[row-calcIndex][col-calcIndex]) {
+					return false;
+				}
+			}else {
+				leftUp = false;
+			}
+			
+			// / 대각선
+			// 우상단
+			if(rightUp && row-calcIndex>=0 && col+calcIndex<P9663.N) {
+				if(P9663.CHESS[row-calcIndex][col+calcIndex]) {
+					return false;
+				}
+			}else {
+				rightUp = false;
+			}
+			
+			// 좌하단
+			if(leftDown && row+calcIndex<P9663.N && col-calcIndex>=0) {
+				if(P9663.CHESS[row+calcIndex][col-calcIndex]) {
+					return false;
+				}
+			}else {
+				leftDown = false;
+			}
+			
+			calcIndex++;
+		}*/
+		
+		
+		// 대각선도 마찬가지로 우하단 쪽은 검사할 필요가 없음
+		int calcIndex = 1;
+		boolean leftUp = true;
+		boolean rightUp = true;
+		boolean leftDown = true;
+		while(leftUp || rightUp || leftDown) {
+			// 좌상단
+			if(leftUp && row-calcIndex>=0 && col-calcIndex>=0) {
+				if(P9663.CHESS[row-calcIndex][col-calcIndex]) {
+					return false;
+				}
+			}else {
+				leftUp = false;
+			}
+			
+			// 우상단
+			if(rightUp && row-calcIndex>=0 && col+calcIndex<P9663.N) {
+				if(P9663.CHESS[row-calcIndex][col+calcIndex]) {
+					return false;
+				}
+			}else {
+				rightUp = false;
+			}
+			
+			// 좌하단
+			if(leftDown && row+calcIndex<P9663.N && col-calcIndex>=0) {
+				if(P9663.CHESS[row+calcIndex][col-calcIndex]) {
+					return false;
+				}
+			}else {
+				leftDown = false;
+			}
+			
+			calcIndex++;
+		}
+		return true;
 	}
 
-	public int calcQueen(int row, int N) {
-		if(row == N) {
+	public int calcQueen(int row) {
+		if(row == P9663.N) {
 			return 1;
 		}
 		
 		int count = 0;
 		
-		for(int col=0; col<N; col++) {
-			if(P9663.CHESS[row][col] == 0) {
-				if(setQueen(row, col, 1)) {
-					count += calcQueen(row+1, N);
-				}
-				setQueen(row, col, 0);
+		for(int col=0; col<P9663.N; col++) {
+			if(isPossiblePosition(row, col)) {
+				P9663.CHESS[row][col] = true;
+				count += calcQueen(row+1);
+				P9663.CHESS[row][col] = false;
 			}
 		}
 		return count;
 	}
 }
+
+
+
